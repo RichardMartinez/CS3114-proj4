717ab4922a96b8192c91d28c4921b0c6f838f69c
RichardMartinez <rmartinez21@vt.edu>
2024-04-20 16:26:43 -0400

    add HashEntryTest

---
+++ S24_P4MemManager/HashEntryTest.java
@@ -0,0 +1,47 @@
+import student.TestCase;
+
+/**
+ * Test the methods of the HashEntry class.
+ * 
+ * @author Richard Martinez
+ * 
+ * @version 2024-04-20
+ */
+public class HashEntryTest extends TestCase {
+
+    private HashEntry entry;
+    
+    private Handle handle;
+    
+    /**
+     * Initialize the test object
+     */
+    public void setUp() {
+        handle = new Handle(15, 25);
+        entry = new HashEntry(5, handle);
+    }
+    
+    /**
+     * Test the methods of HashEntry
+     */
+    public void testHashEntry() {
+        assertEquals(entry.getKey(), 5);
+        assertEquals(entry.getValue().getAddress(), 15);
+        assertEquals(entry.getValue().getLength(), 25);
+        assertEquals(entry.getState(), HashEntryState.EMPTY);
+        
+        entry.setState(HashEntryState.FULL);
+        assertEquals(entry.getState(), HashEntryState.FULL);
+    }
+    
+    /**
+     * Test the default HashEntry
+     */
+    public void testDefaultHashEntry() {
+        entry = new HashEntry();
+        assertEquals(entry.getKey(), -1);
+        assertEquals(entry.getValue().getAddress(), 0);
+        assertEquals(entry.getValue().getLength(), 0);
+        assertEquals(entry.getState(), HashEntryState.EMPTY);
+    }
+}
