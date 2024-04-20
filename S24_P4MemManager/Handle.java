55d2231d5ce9a35b5764e3188dcd99706034594f
RichardMartinez <rmartinez21@vt.edu>
2024-04-20 15:40:06 -0400

    add Handle and HashEntry class

---
+++ S24_P4MemManager/Handle.java
@@ -0,0 +1,13 @@
+
+/**
+ * This class represents a memory handle.
+ * It stores the information used to
+ * retrieve a record from memory
+ * 
+ * @author Richard Martinez
+ * 
+ * @version 2024-04-20
+ */
+public class Handle {
+
+}
---
+++ S24_P4MemManager/HashEntry.java
@@ -0,0 +1,13 @@
+
+/**
+ * This class represents an entry in
+ * the hash table.
+ * It stores a key, value, and a state
+ * 
+ * @author Richard Martinez
+ * 
+ * @version 2024-04-20
+ */
+public class HashEntry {
+
+}
