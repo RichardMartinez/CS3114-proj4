a9b46cf650c34ea2dd8fe6b43d308b9fad27161c
RichardMartinez <rmartinez21@vt.edu>
2024-04-20 15:45:26 -0400

    implement basic Handle class

--- S24_P4MemManager/Handle.java
+++ S24_P4MemManager/Handle.java
@@ -10,4 +10,37 @@
  */
 public class Handle {
 
+    // The starting byte address in memory
+    private int address;
+    
+    // The length of the record in bytes
+    private int length;
+    
+    /**
+     * Constructor for Handle
+     * @param address
+     *      The starting byte address
+     * @param length
+     *      The length of the record in bytes
+     */
+    public Handle(int address, int length) {
+        this.address = address;
+        this.length = length;
+    }
+    
+    /**
+     * Returns the address
+     * @return the address
+     */
+    public int getAddress() {
+        return address;
+    }
+    
+    /**
+     * Returns the length
+     * @return the length
+     */
+    public int getLength() {
+        return length;
+    }
 }
