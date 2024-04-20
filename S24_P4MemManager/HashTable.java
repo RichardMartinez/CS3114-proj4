1b3357b9f9091a1395c3f42148660fc582a7c82b
RichardMartinez <rmartinez21@vt.edu>
2024-04-20 16:46:01 -0400

    add HashTableTest

--- S24_P4MemManager/HashTable.java
+++ S24_P4MemManager/HashTable.java
@@ -10,4 +10,30 @@
  */
 public class HashTable {
 
+    // The capacity of the table
+    private int capacity;
+    
+    // The number of items currently in the table
+    private int size;
+    
+    // The table array
+    private HashEntry[] table;
+    
+    /**
+     * Constructor for HashTable
+     * @param capacity
+     *      The initial capacity must be power of two
+     */
+    public HashTable(int capacity) {
+        // TODO: Check that capacity is power of two
+        this.capacity = capacity;
+        
+        // Init array
+        table = new HashEntry[capacity];
+        
+        // Fill array with default constructed entries
+        for (int i = 0; i < capacity; i++) {
+            table[i] = new HashEntry();
+        }
+    }
 }
---
+++ S24_P4MemManager/HashTableTest.java
@@ -0,0 +1,27 @@
+import student.TestCase;
+
+/**
+ * Test the methods of the HashTable class.
+ * 
+ * @author Richard Martinez
+ * 
+ * @version 2024-04-20
+ */
+public class HashTableTest extends TestCase {
+
+    private HashTable table;
+    
+    /**
+     * Initialize the test object
+     */
+    public void setUp() {
+        table = new HashTable(8);
+    }
+    
+    /**
+     * Test the constructor
+     */
+    public void testConstructor() {
+        int x = 1;
+    }
+}
