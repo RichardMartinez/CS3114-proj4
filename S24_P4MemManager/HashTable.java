93492d0773a601410820a51f2e5bb59d42fabc8f
RichardMartinez <rmartinez21@vt.edu>
2024-05-01 21:33:57 -0400

    HashTable seems working

--- S24_P4MemManager/HashTable.java
+++ S24_P4MemManager/HashTable.java
@@ -12,98 +12,127 @@ public class HashTable {
 
     // The capacity of the table
     private int capacity;
-    
+
     // The number of items currently in the table
     private int size;
-    
+
     // The table array
     private HashEntry[] table;
-    
+
     /**
      * Constructor for HashTable
+     * 
      * @param capacity
-     *      The initial capacity must be power of two
+     *            The initial capacity must be power of two
      */
     public HashTable(int capacity) {
         // TODO: Check that capacity is power of two
         this.capacity = capacity;
-        
+
         // Init array
         table = new HashEntry[capacity];
-        
+
         // Fill array with default constructed entries
         for (int i = 0; i < capacity; i++) {
             table[i] = new HashEntry();
         }
     }
-    
+
+
     /**
      * First hash function h1(k, M)
+     * 
      * @param k
-     *      The integer to hash
+     *            The integer to hash
      * @param M
-     *      The size of the hash table
+     *            The size of the hash table
      * @return the hashed integer
      */
     public int h1(int k, int M) {
         return k % M;
     }
-    
+
+
     /**
      * Second hash function h2(k, M)
+     * 
      * @param k
-     *      The integer to hash
+     *            The integer to hash
      * @param M
-     *      The size of the hash table
+     *            The size of the hash table
      * @return the hashed integer
      */
     public int h2(int k, int M) {
         return (((k / M) % (M / 2)) * 2) + 1;
     }
-    
+
+
     /**
      * Returns true if empty
+     * 
      * @return true if empty
      */
     public boolean isEmpty() {
         return size == 0;
     }
-    
+
+
     /**
      * Return the current size
+     * 
      * @return the size
      */
     public int getSize() {
         return size;
     }
-    
+
+
     /**
      * Return the current capacity
+     * 
      * @return the capacity
      */
     public int getCapacity() {
         return capacity;
     }
-    
+
+
     /**
      * Insert into the hash table
+     * 
      * @param key
-     *      The key to insert
+     *            The key to insert
      * @param value
-     *      The value to insert
+     *            The value to insert
      * @return true on success
      */
     public boolean insert(int key, Handle value) {
         // Assume error checking has been done,
         // Just do the insert
-        
-        // TODO: Check size and resize
-        
+
+        // THIS IS RESIZING BEFORE
+        double ratio = (double)size / (double)capacity;
+        if (ratio >= 0.5) {
+            // Put this contains call here
+            // so it only needs to be called when checking
+            // for resizing (much less often than calling
+            // for every single insert)
+            if (!contains(key)) {
+                // Resize here
+                resize();
+            }
+            else {
+                // It contained it
+                // No duplicate values allowed
+                return false;
+            }
+        }
+
         // Keep track of index and step
         int index = h1(key, capacity);
         int step = h2(key, capacity);
         HashEntryState state = table[index].getState();
-        
+
         // Keep going until you find a valid spot
         while (state != HashEntryState.EMPTY) {
             // Either full or tombstone
@@ -111,82 +140,238 @@ public class HashTable {
                 // We should be able to insert here
                 break;
             }
-            
+
             // This index is full
             // Is there a duplicate key?
             if (table[index].getKey() == key) {
                 // ERROR: Duplicate Key
                 return false;
             }
-            
+
             // No duplicate key, but need to move on
             index = (index + step) % capacity;
             state = table[index].getState();
         }
-        
+
+        // Maybe resize here?
+        // No because then the value of index is now wrong
+
         // Here is a valid index
         HashEntry entry = new HashEntry(key, value);
         entry.setState(HashEntryState.FULL);
         table[index] = entry;
         size++;
-        
         return true;
     }
-    
+
+
+    /**
+     * Removes the key if it exists
+     * 
+     * @param key
+     *            The key to remove
+     * @return true if successful
+     */
+    public boolean remove(int key) {
+        // Assume error checking, just do the remove
+        // Keep track of index and step
+        int index = h1(key, capacity);
+        int step = h2(key, capacity);
+        HashEntryState state = table[index].getState();
+
+        while (state != HashEntryState.EMPTY) {
+            // Either full or tomb
+            if (state == HashEntryState.TOMBSTONE) {
+                // Ignore it
+                index = (index + step) % capacity;
+                state = table[index].getState();
+                continue;
+            }
+
+            // Full
+            if (table[index].getKey() == key) {
+                // Found it!
+                table[index].setState(HashEntryState.TOMBSTONE);
+                size--;
+                return true;
+            }
+
+            // Haven't found it yet
+            index = (index + step) % capacity;
+            state = table[index].getState();
+        }
+
+        return false;
+    }
+
+
     /**
      * Returns the value associated with key if it exists
+     * 
      * @param key
-     *      The key to retrieve
+     *            The key to retrieve
      * @return the value
      */
     public Handle get(int key) {
         // Assume error checking, just do the get
-        
-        // Get the home index
-        int home_index = h1(key, capacity);
-        
-        // Check if its there
-        if (table[home_index].getState() == HashEntryState.FULL) {
-            if (table[home_index].getKey() == key) {
-                // Found it!!
-                return table[home_index].getValue();
+        // Keep track of index and step
+        int index = h1(key, capacity);
+        int step = h2(key, capacity);
+        HashEntryState state = table[index].getState();
+
+        while (state != HashEntryState.EMPTY) {
+            // Full or Tombstone
+            if (state == HashEntryState.TOMBSTONE) {
+                // Ignore it
+                index = (index + step) % capacity;
+                state = table[index].getState();
+                continue;
             }
-        }
-        
-        // Either it was empty, tombstone, or the key didn't match
-        // Do probing
-        int step_size = h2(key, capacity);
-        int probing_index = home_index;  // Need to check home_index for tombstone
-        
-        while (table[probing_index].getState() != HashEntryState.EMPTY) {
-            // Either full or tombstone
-//            if (table[probing_index].getState() == HashEntryState.TOMBSTONE) {
-//                // Skip over it
-//                probing_index = (probing_index + step_size) % capacity;
-//                continue;
-//            }
-            
-            // Here it must be full
-            // Does the key match?
-            if (table[probing_index].getKey() == key) {
-                return table[probing_index].getValue();
+
+            // Full
+            if (table[index].getKey() == key) {
+                // Found it!
+                return table[index].getValue();
             }
-            
-            probing_index = (probing_index + step_size) % capacity;
+
+            // Did not find it yet
+            index = (index + step) % capacity;
+            state = table[index].getState();
         }
-        
-        // At this point, we never found it
+
+        // Did not find it
         return null;
     }
-    
+
+
     /**
      * Returns true if contains key
+     * 
      * @param key
-     *      The key to check
+     *            The key to check
      * @return true if contains
      */
     public boolean contains(int key) {
         return get(key) != null;
     }
-    
+
+
+    /**
+     * Returns the index of the key if it exists
+     * Returns -1 if not found
+     * 
+     * @param key
+     *            The key to search for
+     * @return the index or -1
+     */
+    public int indexOf(int key) {
+        // Keep track of index and step
+        int index = h1(key, capacity);
+        int step = h2(key, capacity);
+        HashEntryState state = table[index].getState();
+
+        while (state != HashEntryState.EMPTY) {
+            // Full or Tombstone
+            if (state == HashEntryState.TOMBSTONE) {
+                // Ignore it
+                index = (index + step) % capacity;
+                state = table[index].getState();
+                continue;
+            }
+
+            // Full
+            if (table[index].getKey() == key) {
+                // Found it!
+                return index;
+            }
+
+            // Did not find it yet
+            index = (index + step) % capacity;
+            state = table[index].getState();
+        }
+
+        // Did not find it
+        return -1;
+    }
+
+
+    /**
+     * Prints the hashtable to standard out
+     */
+    public void print() {
+        System.out.println("Hashtable:");
+
+        String out;
+        HashEntry entry;
+        HashEntryState state;
+
+        // For loop through table array
+        // Print fulls and tombstones
+        for (int i = 0; i < capacity; i++) {
+            entry = table[i];
+            state = entry.getState();
+
+            if (state == HashEntryState.FULL) {
+                out = String.format("%d: %d", i, entry.getKey());
+                System.out.println(out);
+            }
+            else if (state == HashEntryState.TOMBSTONE) {
+                out = String.format("%d: TOMBSTONE", i);
+                System.out.println(out);
+            }
+            // Ignore empty
+        }
+
+        out = String.format("total records: %d", size);
+        System.out.println(out);
+    }
+
+
+    /**
+     * Resize the table by doubling the size
+     * Rehash everything into new capacity
+     */
+    public void resize() {
+        // Double capacity
+        int new_capacity = capacity * 2;
+
+        // Create new size table
+        HashEntry[] new_table = new HashEntry[new_capacity];
+        for (int i = 0; i < new_capacity; i++) {
+            new_table[i] = new HashEntry();
+        }
+
+        // Go through old table
+        // For loop
+        // Re-hash -> Probing -> Insert
+        for (int i = 0; i < capacity; i++) {
+            HashEntry entry = table[i];
+            int key = entry.getKey();
+            HashEntryState state = entry.getState();
+
+            if (state == HashEntryState.FULL) {
+                // Insert into new table
+                int index = h1(key, new_capacity);
+                int step = h2(key, new_capacity);
+                HashEntryState state2 = new_table[index].getState();
+
+                while (state2 != HashEntryState.EMPTY) {
+                    // Never tombstones here, only fulls
+                    // Never duplicates here
+                    index = (index + step) % new_capacity;
+                    state2 = new_table[index].getState();
+                }
+
+                // Found a valid spot here
+                new_table[index] = entry;
+            }
+            // Ignore tombs and empty
+        }
+
+        // Install changes
+        // Size doesn't change
+        table = new_table;
+        capacity = new_capacity;
+    }
+
 }
--- S24_P4MemManager/HashTableTest.java
+++ S24_P4MemManager/HashTableTest.java
@@ -75,45 +75,527 @@ public class HashTableTest extends TestCase {
         success = table.insert(11, handle);
         assertTrue(success);
         
+        assertEquals(table.getSize(), 4);
+        assertEquals(table.getCapacity(), 8);
+        
         // Try inserting duplicate
         handle = new Handle(10, 10);
         success = table.insert(0, handle);
         assertFalse(success);
         
-//        // Test get
-//        handle = table.get(0);
-//        assertEquals(handle.getAddress(), 0);
-//        assertEquals(handle.getAddress(), 0);
-//        
-//        handle = table.get(3);
-//        assertEquals(handle.getAddress(), 3);
-//        assertEquals(handle.getAddress(), 3);
-//        
-//        handle = table.get(8);
-//        assertEquals(handle.getAddress(), 8);
-//        assertEquals(handle.getAddress(), 8);
-//        
-//        handle = table.get(11);
-//        assertEquals(handle.getAddress(), 11);
-//        assertEquals(handle.getAddress(), 11);
-//        
-//        handle = table.get(21);
-//        assertNull(handle);
-//        
-//        handle = table.get(40);
-//        assertNull(handle);
-//        
-//        // Test contains
-//        assertTrue(table.contains(0));
-//        assertTrue(table.contains(3));
-//        assertTrue(table.contains(8));
-//        assertTrue(table.contains(11));
-//        assertFalse(table.contains(21));
-//        assertFalse(table.contains(40));
-//        
-//        // Size methods
-//        assertEquals(table.getSize(), 4);
-//        assertFalse(table.isEmpty());
-
+        assertEquals(table.getSize(), 4);
+        assertEquals(table.getCapacity(), 8);
+        
+        // Test index of
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(3), 3);
+        assertEquals(table.indexOf(8), 6);
+        assertEquals(table.indexOf(11), 1);
+        assertEquals(table.indexOf(1), -1);
     }
+    
+    /**
+     * Test the get method
+     */
+    public void testGet() {
+        Handle handle;
+        
+        assertTrue(table.isEmpty());
+        
+        handle = table.get(0);
+        assertNull(handle);
+        assertEquals(table.indexOf(0), -1);
+        
+        handle = new Handle(0, 0);
+        table.insert(0, handle);
+        
+        handle = new Handle(3, 3);
+        table.insert(3, handle);
+        
+        handle = new Handle(8, 8);
+        table.insert(8, handle);
+        
+        handle = new Handle(11, 11);
+        table.insert(11, handle);
+        
+        handle = table.get(0);
+        assertEquals(handle.getAddress(), 0);
+        assertEquals(handle.getLength(), 0);
+        
+        handle = table.get(3);
+        assertEquals(handle.getAddress(), 3);
+        assertEquals(handle.getLength(), 3);
+        
+        handle = table.get(8);
+        assertEquals(handle.getAddress(), 8);
+        assertEquals(handle.getLength(), 8);
+        
+        handle = table.get(11);
+        assertEquals(handle.getAddress(), 11);
+        assertEquals(handle.getLength(), 11);
+        
+        // Test contains
+        assertTrue(table.contains(0));
+        assertTrue(table.contains(3));
+        assertTrue(table.contains(8));
+        assertTrue(table.contains(11));
+        assertFalse(table.contains(1));
+        
+        // Test size methods
+        assertEquals(table.getSize(), 4);
+        assertFalse(table.isEmpty());
+    }
+    
+    /**
+     * Test the print method
+     */
+    public void testPrint() {
+        Handle handle;
+        
+        handle = new Handle(0, 0);
+        table.insert(0, handle);
+        
+        handle = new Handle(3, 3);
+        table.insert(3, handle);
+        
+        handle = new Handle(8, 8);
+        table.insert(8, handle);
+        
+        handle = new Handle(11, 11);
+        table.insert(11, handle);
+        
+        // Test print
+        systemOut().clearHistory();
+        table.print();
+        String actual = systemOut().getHistory();
+        
+        String expected = "Hashtable:\n" +
+            "0: 0\n" +
+            "1: 11\n" +
+            "3: 3\n" +
+            "6: 8\n" +
+            "total records: 4\n";
+        
+        assertFuzzyEquals(actual, expected);
+    }
+    
+    
+    /**
+     * Test the remove method
+     */
+    public void testRemove() {
+        Handle handle;
+        int key;
+        boolean success;
+        
+        assertTrue(table.isEmpty());
+        
+        success = table.remove(0);
+        assertFalse(success);
+        assertTrue(table.isEmpty());
+        
+        key = 0;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        assertEquals(table.getSize(), 1);
+        
+        key = 3;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        assertEquals(table.getSize(), 2);
+        
+        key = 8;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        assertEquals(table.getSize(), 3);
+        
+        key = 11;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        assertEquals(table.getSize(), 4);
+        
+        // Test remove simple
+        success = table.remove(3);
+        assertTrue(success);
+        assertEquals(table.getSize(), 3);
+        
+        // Test double remove
+        success = table.remove(3);
+        assertFalse(success);
+        assertEquals(table.getSize(), 3);
+        
+        // Test remove non existent
+        success = table.remove(1);
+        assertFalse(success);
+        assertEquals(table.getSize(), 3);
+        
+        // Test remove simple again
+        success = table.remove(11);
+        assertTrue(success);
+        assertEquals(table.getSize(), 2);
+        
+        // Verify structure
+        systemOut().clearHistory();
+        table.print();
+        String actual = systemOut().getHistory();
+        
+        String expected = "Hashtable:\n" +
+            "0: 0\n" +
+            "1: TOMBSTONE\n" +
+            "3: TOMBSTONE\n" +
+            "6: 8\n" +
+            "total records: 2\n";
+        
+        assertFuzzyEquals(actual, expected);
+    }
+    
+    /**
+     * Test inserting into a spot where a tomb is
+     */
+    public void testInsertIntoTombstone() {
+        Handle handle;
+        
+        handle = new Handle(0, 0);
+        table.insert(0, handle);
+        
+        handle = new Handle(3, 3);
+        table.insert(3, handle);
+        
+        handle = new Handle(8, 8);
+        table.insert(8, handle);
+        
+        handle = new Handle(11, 11);
+        table.insert(11, handle);
+        
+        int elevenIndex = table.indexOf(11);
+        
+        // Remove 11 add 1
+        table.remove(11);
+        
+        handle = new Handle(1, 1);
+        table.insert(1, handle);
+        
+        int oneIndex = table.indexOf(1);
+        
+        assertEquals(elevenIndex, oneIndex);
+        
+        assertEquals(table.getSize(), 4);
+    }
+    
+    /**
+     * Test ignoring tombs in get
+     */
+    public void testGetIgnoreTombstone() {
+        Handle handle;
+        
+        handle = new Handle(0, 0);
+        table.insert(0, handle);
+        
+        handle = new Handle(3, 3);
+        table.insert(3, handle);
+        
+        handle = new Handle(8, 8);
+        table.insert(8, handle);
+        
+        handle = new Handle(11, 11);
+        table.insert(11, handle);
+        
+        table.remove(8);
+        
+        handle = table.get(11);
+        assertEquals(handle.getAddress(), 11);
+        assertEquals(handle.getLength(), 11);
+        
+        assertEquals(table.getSize(), 3);
+    }
+    
+    /**
+     * Test ignoring tombs in indexOf
+     */
+    public void testIndexOfIgnoreTombstone() {
+        Handle handle;
+        
+        handle = new Handle(0, 0);
+        table.insert(0, handle);
+        
+        handle = new Handle(3, 3);
+        table.insert(3, handle);
+        
+        handle = new Handle(8, 8);
+        table.insert(8, handle);
+        
+        handle = new Handle(11, 11);
+        table.insert(11, handle);
+        
+        table.remove(8);
+        
+        int index = table.indexOf(11);
+        assertEquals(index, 1);
+        
+        assertEquals(table.getSize(), 3);
+    }
+    
+    /**
+     * Test the resize method
+     */
+    public void testResize() {
+        Handle handle;
+        
+        handle = new Handle(0, 0);
+        table.insert(0, handle);
+        
+        handle = new Handle(3, 3);
+        table.insert(3, handle);
+        
+        handle = new Handle(8, 8);
+        table.insert(8, handle);
+        
+        handle = new Handle(11, 11);
+        table.insert(11, handle);
+        
+        // Verify structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(3), 3);
+        assertEquals(table.indexOf(8), 6);
+        assertEquals(table.indexOf(11), 1);
+        
+        // Do the resize
+        assertEquals(table.getCapacity(), 8);
+        assertEquals(table.getSize(), 4);
+        table.resize();
+        assertEquals(table.getCapacity(), 16);
+        assertEquals(table.getSize(), 4);
+        
+        // Verify structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(3), 3);
+        assertEquals(table.indexOf(8), 8);
+        assertEquals(table.indexOf(11), 11);
+    }
+    
+    /**
+     * Test the resize method with another table
+     */
+    public void testResize2() {
+        Handle handle;
+        int key;
+        
+        key = 100;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 200;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 300;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 400;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        // Verify the structure
+        assertEquals(table.indexOf(100), 4);
+        assertEquals(table.indexOf(200), 0);
+        assertEquals(table.indexOf(300), 7);
+        assertEquals(table.indexOf(400), 5);
+        
+        // Do the resize
+        assertEquals(table.getCapacity(), 8);
+        assertEquals(table.getSize(), 4);
+        table.resize();
+        assertEquals(table.getCapacity(), 16);
+        assertEquals(table.getSize(), 4);
+        
+        // Verify the structure
+        assertEquals(table.indexOf(100), 4);
+        assertEquals(table.indexOf(200), 8);
+        assertEquals(table.indexOf(300), 12);
+        assertEquals(table.indexOf(400), 0);
+    }
+    
+    /**
+     * Test resize with probing in new table
+     */
+    public void testResize3() {
+        Handle handle;
+        int key;
+        
+        key = 0;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 10;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 20;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 32;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        // Verify the structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(10), 2);
+        assertEquals(table.indexOf(20), 4);
+        assertEquals(table.indexOf(32), 1);
+        
+        // Do the resize
+        assertEquals(table.getCapacity(), 8);
+        assertEquals(table.getSize(), 4);
+        table.resize();
+        assertEquals(table.getCapacity(), 16);
+        assertEquals(table.getSize(), 4);
+        
+        // Verify the structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(10), 10);
+        assertEquals(table.indexOf(20), 4);
+        assertEquals(table.indexOf(32), 5);
+    }
+    
+    /**
+     * Test resize ignoring tombstones
+     */
+    public void testResizeIgnoreTombstone() {
+        Handle handle;
+        int key;
+        
+        key = 0;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 10;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 20;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 32;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        table.remove(20);
+        
+        // Verify the structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(10), 2);
+        assertEquals(table.indexOf(32), 1);
+        
+        // Do the resize
+        assertEquals(table.getCapacity(), 8);
+        assertEquals(table.getSize(), 3);
+        table.resize();
+        assertEquals(table.getCapacity(), 16);
+        assertEquals(table.getSize(), 3);
+        
+        // Verify the structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(10), 10);
+        assertEquals(table.indexOf(32), 5);
+    }
+    
+    /**
+     * Test the resizing before strategy
+     */
+    public void testResizingBefore() {
+        Handle handle;
+        int key;
+        
+        key = 0;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 3;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 8;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 12;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        assertEquals(table.getCapacity(), 8);
+        
+        // Verify Structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(3), 3);
+        assertEquals(table.indexOf(8), 6);
+        assertEquals(table.indexOf(12), 4);
+        
+        // This should cause a resize
+        key = 28;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        assertEquals(table.getCapacity(), 16);
+        assertEquals(table.getSize(), 5);
+        
+        // Verify Structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(3), 3);
+        assertEquals(table.indexOf(8), 8);
+        assertEquals(table.indexOf(12), 12);
+        assertEquals(table.indexOf(28), 15);
+    }
+    
+    /**
+     * Test ignoring duplicate insert not affecting capacity
+     */
+    public void testResizingBeforeIgnoreDuplicate() {
+        Handle handle;
+        int key;
+        
+        key = 0;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 3;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 8;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        key = 12;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        assertEquals(table.getCapacity(), 8);
+        
+        // Verify Structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(3), 3);
+        assertEquals(table.indexOf(8), 6);
+        assertEquals(table.indexOf(12), 4);
+        
+        assertEquals(table.getCapacity(), 8);
+        assertEquals(table.getSize(), 4);
+        
+        // This should NOT cause a resize
+        key = 0;
+        handle = new Handle(key, key);
+        table.insert(key, handle);
+        
+        assertEquals(table.getCapacity(), 8);
+        assertEquals(table.getSize(), 4);
+        
+        // Verify Structure
+        assertEquals(table.indexOf(0), 0);
+        assertEquals(table.indexOf(3), 3);
+        assertEquals(table.indexOf(8), 6);
+        assertEquals(table.indexOf(12), 4);
+    }
+    
 }
